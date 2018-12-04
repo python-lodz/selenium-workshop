bd12243ae490e3ea32478dbbb5bd1208bdd4162a
Sebastian Buczyński <nnplaya@gmail.com>
2018-12-04 19:03:46 +0100

    Zadanie 4

--- zadanie4.py
+++ zadanie4.py
@@ -4,6 +4,19 @@ from selenium import webdriver
 
 
 class WorkshopTests(unittest.TestCase):
+    """
+    Zrefaktoryzuj zadanie 3 i utwórz Page Objects dla:
+    - formularza wyszukiwania (wpisz miejsce, wybierz przedział dat)
+    - filtrów przedziału cenowego na stronie wyników
+    - filtrów ilości gwiazdek na stronie wyników
+
+    Korzystając z gotowych komponentów, napisz dwa scenariusze (metody) testowe:
+    - sprawdzający dostępność obiektów o 3 gwiazdkach w marcu 2018
+    - sprawdzający że w weekend 15-16. grudnia na Mahattanie nie ma więcej niż 10 dostępnych ofert w najniższym przedziale cenowym
+
+    Page objects:
+    https://selenium-python.readthedocs.io/page-objects.html
+    """
 
     def setUp(self):
         self.driver = webdriver.Chrome()
