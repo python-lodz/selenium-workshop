b7ba2bf020052eaaf57524cad2e59f6184cdfc24
Sebastian Buczyński <nnplaya@gmail.com>
2018-12-04 18:55:22 +0100

    Zadanie 3

--- zadanie3.py
+++ zadanie3.py
@@ -1,9 +1,18 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 
 class WorkshopTests(unittest.TestCase):
+    """
+    Korzystając ze strony booking.com, znajdź nocleg
+    w Nowym Jorku na 6-11. stycznia 2019 na Manhattanie w najniższej dostępnej półce cenowej.
+
+    https://selenium-python.readthedocs.io/waits.html
+    """
 
     def setUp(self):
         self.driver = webdriver.Chrome()
@@ -12,7 +21,7 @@ class WorkshopTests(unittest.TestCase):
         self.driver.quit()
 
     def test_exercise(self):
-        pass
+        self.driver.get('https://booking.com')
 
 
 if __name__ == "__main__":
