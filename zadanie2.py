bf8d70b728d37355eb631d18e7f3791e6dd41b74
olektomek <olekstomek@gmail.com>
2018-12-07 22:32:22 +0100

    zadanie2.py - rozwiązanie

--- zadanie2.py
+++ zadanie2.py
@@ -29,11 +29,41 @@ class WorkshopTests(unittest.TestCase):
 
     def tearDown(self):
         self.driver.quit()
-
+    # https://www.paste.org/96245 - próba wykonania drag and drop bez sukcesu.
+    # Element jest łapany i przeciągany ale nie jest upuszczany
     def test_exercise(self):
-        self.driver.get('Z POPRZEDNIEGO ZADANIA')
+            self.driver.get('https://breadcrumbscollector.tech/pl/selenium/2223.html')
 
-        assert self.driver.find_element_by_id('recipe').text == 'Gotowe!'
+            recipe = self.driver.find_element_by_id('recipe').text
+        
+            while(not recipe in 'Gotowe!'):
+                action_chains = ActionChains(self.driver)
+                if recipe == 'Gotuj na wolnym ogniu':
+                    self.driver.find_element_by_css_selector('#heating-control > label:nth-child(1)').click()
+                    recipe = self.driver.find_element_by_id('recipe').text
+                    continue
+                elif recipe == 'Gotuj na dużym ogniu':
+                    self.driver.find_element_by_css_selector('#heating-control > label:nth-child(3)').click()
+                    recipe = self.driver.find_element_by_id('recipe').text
+                    continue
+                elif recipe == 'Wrzuć ziemniaka':
+                    action_chains.double_click(self.driver.find_element_by_id('potato')).perform()
+                    recipe = self.driver.find_element_by_id('recipe').text
+                    continue
+                elif recipe == 'Wrzuć marchewkę':
+                    action_chains.double_click(self.driver.find_element_by_id('carrot')).perform()
+                    recipe = self.driver.find_element_by_id('recipe').text
+                    continue
+                elif recipe == 'Wrzuć sałatę':
+                    action_chains.double_click(self.driver.find_element_by_id('lettuce')).perform()
+                    recipe = self.driver.find_element_by_id('recipe').text
+                    continue
+                elif recipe == 'Wrzuć mięso':
+                    action_chains.double_click(self.driver.find_element_by_id('meat')).perform()
+                    recipe = self.driver.find_element_by_id('recipe').text
+                    continue
+                
+            assert self.driver.find_element_by_id('recipe').text == 'Gotowe!'
 
 
 if __name__ == "__main__":
